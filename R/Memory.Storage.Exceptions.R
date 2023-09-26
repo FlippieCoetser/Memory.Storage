dba2e645f50fa97e0e4e3089b29f55f77db7d449
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-26 05:59:00 -0700

    EXCEPTIONS: Memory.Storage.Exceptions() ReturnsListContainingInvalidIdentifier -> PASS

--- R/Memory.Storage.Exceptions.R
+++ R/Memory.Storage.Exceptions.R
@@ -1,24 +1,25 @@
 Memory.Storage.Exceptions <- \() {
   exceptions <- list()
-  exceptions[['NoExecuteQuery']] <- \(invoke) {
+  exceptions[['NoExecuteQuery']]    <- \(invoke) {
     if(invoke) {
       stop('Memory Storage Provider Error: ExecuteQuery not implemented.', call. = FALSE)
     }
   }
-  exceptions[['DuplicateId']]    <- \(invoke) {
+  exceptions[['DuplicateId']]       <- \(invoke) {
     if(invoke) {
       stop('Memory Storage Provider Error: Duplicate Id not allowed.', call. = FALSE)
     }
   }
-  exceptions[['EntityNotFound']] <- \(invoke) {
+  exceptions[['EntityNotFound']]    <- \(invoke) {
     if(invoke) {
       stop('Memory Storage Provider Error: Entity not found.', call. = FALSE)
     }
   }
-  exceptions[['InvalidTable']]   <- \(invoke, table) {
+  exceptions[['InvalidTable']]      <- \(invoke, table) {
     if(invoke) {
       stop('Memory Storage Provider Error: ', table, ' is not a valid table.', call. = FALSE)
     }
   }
+  exceptions[['InvalidIdentifier']] <- \() {}
   return(exceptions)
 }
